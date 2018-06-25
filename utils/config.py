ef5eb27161f09a226dade5355195c374c475b136
xiaoqiang <xiaoqiang@bwbot.org>
2018-06-25 11:12:23 +0800

    change matrix

--- utils/config.py
+++ utils/config.py
@@ -6,10 +6,10 @@ HOST = ''  # should not be 127.0.0.1 or localhost
 USERSOCKET_PORT = 20001  # 局域网udp命令监听端口
 BROADCAST_PORT = 22001
 
-MAX_VEL = 0.8
+MAX_VEL = 1.3
 MAX_THETA = 3.0
 POWER_LOW = 10.0
 
 TF_ROT = np.array([[0., 0., 1.],
                    [-1., 0., 0.], [0., -1., 0.]])
-TF_TRANS = np.array([0.05, 0.0, 0.])
+TF_TRANS = np.array([0.33, 0.0, 0.])
