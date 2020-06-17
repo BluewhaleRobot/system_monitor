fccf067c343adef8e5beb8082b5e6242069a8c8c
xiaoqiang <xiaoqiang@bwbot.org>
2020-06-17 15:48:58 +0800

    add map and path info in log

--- src/system_monitor/system_logger_node.py
+++ src/system_monitor/system_logger_node.py
@@ -6,6 +6,8 @@ from galileo_serial_server.msg import GalileoStatus
 from xiaoqiang_log.msg import LogRecord
 import time
 import json
+import os
+import hashlib
 
 previous_status = None
 log_pub = None
@@ -18,6 +20,40 @@ task_record = {
     "events": [],
     "results": ""
 }
+
+def get_current_map_info():
+    MAP_WORKSPACE_PATH = "/home/xiaoqiang/slamdb"
+    if not os.path.exists(os.path.join(MAP_WORKSPACE_PATH, "map_name.txt")):
+        return None
+    # 获取地图名称
+    map_name = ""
+    with open(os.path.join(MAP_WORKSPACE_PATH, "map_name.txt"), 'r') as map_name_file:
+        map_name = map_name_file.read().strip()
+    # 计算路径文件md5值
+    path_name = ""
+    if not os.path.exists(os.path.join(MAP_WORKSPACE_PATH, "nav.csv")):
+        return {
+            "map_name": map_name,
+            "path_name": path_name
+        }
+    with open(os.path.join(MAP_WORKSPACE_PATH, "nav.csv"), 'rb') as nav_file:
+        data = nav_file.read()
+    nav_file_md5= hashlib.md5(data).hexdigest()
+    map_files = os.listdir(MAP_WORKSPACE_PATH)
+    for map_file in map_files:
+        if map_file.startswith("nav_"):
+            with open(os.path.join(MAP_WORKSPACE_PATH, map_file), 'rb') as path_file:
+                data = path_file.read()
+                path_file_md5 = hashlib.md5(data).hexdigest()
+                if path_file_md5 == nav_file_md5:
+                    return {
+                        "map_name": map_name,
+                        "path_name": map_file[4:-4]
+                    }
+    return {
+        "map_name": map_name,
+        "path_name": path_name
+    }
 
 def galileo_status_logger(status):
     global previous_status, work_stamp_start, pause_stamp_start, log_pub, task_record
@@ -32,9 +68,14 @@ def galileo_status_logger(status):
 
     # 从work状态到free状态,正常完成后targetNumID不为,如果取消任务则为-1
     if previous_status.targetStatus == 1 and status.targetStatus == 0 and status.targetNumID != -1:
+        current_map_info = get_current_map_info()
         task_record["events"].append({
             "type": "WORKING",
-            "duration": current_stamp - work_stamp_start
+            "duration": current_stamp - work_stamp_start,
+            "timestamp": int(time.time() * 1000),
+            "target": status.targetNumID,
+            "map": "" if current_map_info is None else current_map_info["map_name"],
+            "path": "" if current_map_info is None else current_map_info["path_name"]
         })
         task_record["results"] = "SUCCESS"
 
@@ -57,27 +98,42 @@ def galileo_status_logger(status):
     
     # 从working状态到pause状态
     if previous_status.targetStatus == 1 and status.targetStatus == 2:
+        current_map_info = get_current_map_info()
         task_record["events"].append({
             "type": "WORKING",
             "duration": current_stamp - work_stamp_start,
+            "timestamp": int(time.time() * 1000),
+            "target": status.targetNumID,
+            "map": "" if current_map_info is None else current_map_info["map_name"],
+            "path": "" if current_map_info is None else current_map_info["path_name"]
         })
         pause_stamp_start = current_stamp
         work_stamp_start = 0
 
     # 从pause状态到working状态
     if previous_status.targetStatus == 2 and status.targetStatus == 1:
+        current_map_info = get_current_map_info()
         task_record["events"].append({
             "type": "PAUSED",
-            "duration": current_stamp - pause_stamp_start
+            "duration": current_stamp - pause_stamp_start,
+            "timestamp": int(time.time() * 1000),
+            "target": status.targetNumID,
+            "map": "" if current_map_info is None else current_map_info["map_name"],
+            "path": "" if current_map_info is None else current_map_info["path_name"]
         })
         work_stamp_start = current_stamp
         pause_stamp_start = 0
 
     # 从working状态到cancel状态
     if previous_status.targetStatus == 1 and status.targetStatus == 0 and status.targetNumID == -1:
+        current_map_info = get_current_map_info()
         task_record["events"].append({
             "type": "WORKING",
             "duration": current_stamp - work_stamp_start,
+            "timestamp": int(time.time() * 1000),
+            "target": status.targetNumID,
+            "map": "" if current_map_info is None else current_map_info["map_name"],
+            "path": "" if current_map_info is None else current_map_info["path_name"]
         })
         task_record["results"] = "CANCEL"
 
@@ -99,9 +155,14 @@ def galileo_status_logger(status):
 
     # 从pause状态到cancel状态
     if previous_status.targetStatus == 2 and status.targetStatus == 0:
+        current_map_info = get_current_map_info()
         task_record["events"].append({
             "type": "PAUSED",
-            "duration": current_stamp - pause_stamp_start
+            "duration": current_stamp - pause_stamp_start,
+            "timestamp": int(time.time() * 1000),
+            "target": status.targetNumID,
+            "map": "" if current_map_info is None else current_map_info["map_name"],
+            "path": "" if current_map_info is None else current_map_info["path_name"]
         })
         task_record["results"] = "CANCEL"
 
@@ -124,6 +185,7 @@ def galileo_status_logger(status):
 
 
 
+    
 
 
 if __name__ == "__main__":
