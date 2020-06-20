380c7b2ea373253e1d41f9f6754eea33d59e5be0
xiaoqiang <xiaoqiang@bwbot.org>
2020-06-20 11:22:30 +0800

    修复雷达可能异常停止的bug

--- src/system_monitor/status_audio_reporter.py
+++ src/system_monitor/status_audio_reporter.py
@@ -68,7 +68,7 @@ if __name__ == "__main__":
                 #audio_pub.publish("好的，我回去了，您慢用！")
                 pass
             # 被挡住提示
-            if not MOVE_FLAG or (status.targetStatus == 1 and abs(status.currentSpeedX) < 0.01 and abs(status.currentSpeedTheta) < 0.01):
+            if status.targetStatus == 1 and (abs(status.currentSpeedX) < 0.01 and abs(status.currentSpeedTheta) < 0.01):
                 # 被人挡住了,在 WORKING 状态但是没有动
                 BLOCK_TIME_COUNT += (1000 / 30)
             else:
@@ -112,7 +112,7 @@ if __name__ == "__main__":
                 WARN_TIME_COUNT = 0
 
             # 5min不动则关闭雷达
-            if abs(status.currentSpeedX) < 0.01 and abs(status.currentSpeedTheta) < 0.01:
+            if PREVISOUS_STATUS.navStatus==1 and status.targetStatus != 1 and abs(status.currentSpeedX) < 0.01 and abs(status.currentSpeedTheta) < 0.01:
                 STOP_TIME_COUNT += (1000 / 30)
             else:
                 STOP_TIME_COUNT = 0
