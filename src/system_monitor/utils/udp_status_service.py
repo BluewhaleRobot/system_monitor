839545f8add3317eb61b568166572e25908dcd1e
xiaoqiang <xiaoqiang@bwbot.org>
2020-08-10 22:28:06 +0800

    first version

--- src/system_monitor/utils/udp_status_service.py
+++ src/system_monitor/utils/udp_status_service.py
@@ -34,21 +34,29 @@ import tf
 from galileo_serial_server.msg import GalileoStatus
 from geometry_msgs.msg import Pose, PoseStamped, Twist
 from nav_msgs.msg import Odometry
+from bw_env_sensors.msg import EnvSensors
 
 from config import BROADCAST_PORT, ROS_PACKAGE_PATH, TF_ROT, TF_TRANS
 
 
 class UDPStatusService(threading.Thread):
 
-    def __init__(self, monitor_server, galileo_status, galileo_status_lock):
+    def __init__(self, monitor_server, galileo_status, galileo_status_lock, useEnvSensors):
         super(UDPStatusService, self).__init__()
         # stop flag
         self._stop = threading.Event()
         self._stop.set()
         self.listener = tf.TransformListener(True, rospy.Duration(10.0))
         self.send_data = bytearray(
-            [205, 235, 215, 36, 0x00, 0x00, 0x00, 0x00,
+            [205, 235, 215, 44, 0x00, 0x00, 0x00, 0x00,
              0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+             0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+             0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+             0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+             0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+             ])
+        self.send_data2 = bytearray(
+            [205, 235, 215, 32, 0x00, 0x00, 0x00, 0x00,
              0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
              0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
              0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
@@ -58,8 +66,27 @@ class UDPStatusService(threading.Thread):
         self.robot_pose_stamped = None
         self.galileo_status = galileo_status
         self.galileo_status_lock = galileo_status_lock
-        self.odom_flag = False
 
+        self.env_sensor_data = EnvSensors()
+
+        def update_envSensor(sensorData):
+            self.env_sensor_data.header = sensorData.header
+            self.env_sensor_data.temperature = sensorData.temperature #Centigrade
+            self.env_sensor_data.rh = sensorData.rh #relative humidity %RH
+            self.env_sensor_data.smoke = sensorData.smoke #ppm
+            self.env_sensor_data.pm1_0 = sensorData.pm1_0 #ug/m^3
+            self.env_sensor_data.pm2_5 = sensorData.pm2_5 #ug/m^3
+            self.env_sensor_data.pm10 = sensorData.pm10 #ug/m^3
+            self.env_sensor_data.lel = sensorData.lel #ppm
+            self.env_sensor_data.noise = sensorData.noise #db
+
+        if useEnvSensors == 0:
+            self.envSensor_flag = False
+        else:
+            self.envSensor_sub = rospy.Subscriber("/bw_env_sensors/EnvSensorData", EnvSensors, update_envSensor)
+            self.envSensor_flag = True
+
+        self.odom_flag = False
         def update_odom(odom):
             self.odom_flag = True
             self.robot_pose_stamped = PoseStamped()
@@ -78,6 +105,7 @@ class UDPStatusService(threading.Thread):
     def run(self):
         rate = rospy.Rate(30)
         self._stop.clear()
+        num_i = 0
         while not self.stopped() and not rospy.is_shutdown():
             with self.galileo_status_lock:
                 self.send_data[4:8] = map(ord, struct.pack('f', self.galileo_status.currentPosX))
@@ -95,6 +123,8 @@ class UDPStatusService(threading.Thread):
                     'i', self.galileo_status.loopStatus))
                 self.send_data[36:40] = map(ord, struct.pack(
                     'i', self.galileo_status.chargeStatus))
+                self.send_data[44:48] = map(
+                    ord, struct.pack('f', self.galileo_status.targetDistance))
 
                 if self.galileo_status.navStatus == 1:
                     statu0 = 0x01  # 导航状态
@@ -124,4 +154,20 @@ class UDPStatusService(threading.Thread):
                 statu2 + statu3 + status4 + status5
             self.send_data[3] = len(self.send_data) - 4
             self.monitor_server.sendto(bytes(self.send_data))
+
+            if self.envSensor_flag and  num_i >= 3:
+                num_i = 0
+                self.send_data2[4:8] = map(ord, struct.pack('f', self.env_sensor_data.temperature))
+                self.send_data2[8:12] = map(ord, struct.pack('f', self.env_sensor_data.rh))
+                self.send_data2[12:16] = map(ord, struct.pack('f', self.env_sensor_data.smoke))
+                self.send_data2[16:20] = map(ord, struct.pack('f', self.env_sensor_data.pm1_0))
+                self.send_data2[20:24] = map(ord, struct.pack('f', self.env_sensor_data.pm2_5))
+                self.send_data2[24:28] = map(ord, struct.pack('f', self.env_sensor_data.pm10))
+                self.send_data2[28:32] = map(ord, struct.pack('f', self.env_sensor_data.lel))
+                self.send_data2[32:36] = map(ord, struct.pack('f', self.env_sensor_data.noise))
+                self.send_data2[3] = len(self.send_data2) - 4
+                self.monitor_server.sendto(bytes(self.send_data2))
+                # l = [hex(int(j)) for j in self.send_data2]
+                # print(" ".join(l))
+            num_i = num_i +1
             rate.sleep()
